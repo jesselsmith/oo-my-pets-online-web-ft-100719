67c8f99ecf7b352e59d9d955271fbb1aa5d19995
Jesse Smith <jesse.lee.smith@gmail.com>
2019-10-15 16:27:52 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,8 +1,8 @@
 class Owner
   attr_reader :name
   def initialize(name)
-    self.name = name
-    self.name.freeze
+    @name = name
+    @name.freeze
     @species = "Human"
     @species.freeze
   end
