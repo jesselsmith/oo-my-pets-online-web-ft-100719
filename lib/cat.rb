b50304f68f90fb0b41bb14bb3c10f4faee8b1e22
Jesse Smith <jesse.lee.smith@gmail.com>
2019-10-15 16:37:02 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,9 +1,20 @@
 class Cat
   attr_accessor :owner, :mood
   attr_reader :name
+  @@all = []
 
   def initialize(name, owner)
     @name = name
     self.owner = owner
     self.mood = "nervous"
+    self.save
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
 end
