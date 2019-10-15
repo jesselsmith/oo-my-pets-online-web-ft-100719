0030e8f735e6e73f6bc3a5980ccfda541549b9ef
Jesse Smith <jesse.lee.smith@gmail.com>
2019-10-15 17:49:54 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -36,13 +36,21 @@ class Owner
     Dog.all.select{|dog| dog.owner = self}
   end
 
-  def buy_cat(cat)
-    if self.pets.has_key?(:Cats)
-      pets[:Cats] << cat
+  def buy_pet(pet, pet_category)
+    if self.pets.has_key?(pet_category)
+      pets[pet_category] << pet
     else
-      pets[:Cats] = [cat]
+      pets[pet_category] = [pet]
     end
-    cat.owner = self
+    pet.owner = self
+  end
+
+  def buy_cat(cat)
+    buy_pet(cat, :Cats)
+  end
+
+  def buy_dog(dog)
+    buy_pet(dog, :Dogs)
   end
 
 end
